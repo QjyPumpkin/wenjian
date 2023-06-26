f7260eff9584f879de8f40a911306269a016e7d8
QjyPumpkin <120186386+QjyPumpkin@users.noreply.github.com>
2023-06-26 18:57:48 +0200

    update

--- test2.py
+++ test2.py
@@ -5,86 +5,89 @@ import casadi as ca
 import time as time
 from datetime import datetime
 
-A = np.random.randint(1, 100, (4,5,3))
-# A = np.random.rand(4,5,3)
-print('A \n', A)   
+class data_test:
+    def matrix(self,data):
+    # reshape Hankel Matrix to the right form 
+        B = []  # e.g. U_p(6,301,3) --> (6*3, 301)
+        C = []
+        num_rows = data.shape[0] * data.shape[2]
+        num_cols = data.shape[1]
+        
+        # Reshape the data to (num_rows, num_cols)
+        B = data.transpose(0, 2, 1)
+        C = B.reshape((num_rows, num_cols))
+        return C 
 
-# C 
-#  [[62 43 63 58 98 69 14 52 86 55 17 84]
-#  [12 97 83 12 80 31 31 90 41 11 23 10]
-#  [92 19 60 52 45  3 25 66 49 14  4 42]
-#  [ 9 97 45 59 10 66 23 35 49  7 73 95]
-#  [20 38  8 85 35  1 70 12 63 41 20 81]]
+
+if __name__ == '__main__':
+    A = np.random.randint(1, 100, (4,5,3))
+    # A = np.random.rand(4,5,3)
+    print('A \n', A)   
+    obj = data_test()
+    B = obj.matrix(A)
+    print("B \n", B)
 
 # A 
-#  [[[62 43 63]
-#   [58 98 69]
-#   [14 52 86]
-#   [55 17 84]
-#   [12 97 83]]
+#  [[[50 30  3]
+#   [42 64 34]
+#   [99 45 99]
+#   [84  4 12]
+#   [87 41 89]]
 
-#  [[12 80 31]
-#   [31 90 41]
-#   [11 23 10]
-#   [92 19 60]
-#   [52 45  3]]
+#  [[61 34 38]
+#   [93 31 95]
+#   [81 94 92]
+#   [86 75 25]
+#   [85 55 24]]
 
-#  [[25 66 49]
-#   [14  4 42]
-#   [ 9 97 45]
-#   [59 10 66]
-#   [23 35 49]]
+#  [[30 54 76]
+#   [10 42 90]
+#   [96 92 45]
+#   [42 27 15]
+#   [33 89 74]]
 
-#  [[ 7 73 95]
-#   [20 38  8]
-#   [85 35  1]
-#   [70 12 63]
-#   [41 20 81]]]
-
-
-
-
-# B = np.reshape(A,(-1,2))
-# print(B)
-# print('B的维度:', B.shape)
-B = np.reshape(A,(-1,1))
-print('B \n', B)
-print('B的维度:', B.shape)
-
-C = np.reshape(B,(5,12))
-print('C \n', C)
-print('C的维度:', C.shape)
-
+#  [[96 30 48]
+#   [95 34 44]
+#   [34 34 11]
+#   [29 79 51]
+#   [96  3 41]]]
+# B 
+#  [[50 42 99 84 87]
+#  [30 64 45  4 41]
+#  [ 3 34 99 12 89]
+#  [61 93 81 86 85]
+#  [34 31 94 75 55]
+#  [38 95 92 25 24]
+#  [30 10 96 42 33]
+#  [54 42 92 27 89]
+#  [76 90 45 15 74]
+#  [96 95 34 29 96]
+#  [30 34 34 79  3]
+#  [48 44 11 51 41]]
 
 
-# [[[ 0  1  2]
-#   [ 3  4  5]
-#   [ 6  7  8]]
 
-#  [[ 9 10 11]
-#   [12 13 14]
-#   [15 16 17]]]
-# [[ 0  1]
-#  [ 2  3]
-#  [ 4  5]
-#  [ 6  7]
-#  [ 8  9]
-#  [10 11]
-#  [12 13]
-#  [14 15]
-#  [16 17]]
-# B的维度: (9, 2)
+    # B = np.reshape(A,(-1,2))
+    # print(B)
+    # print('B的维度:', B.shape)
+    # B = np.reshape(A,(-1,1))
+    # print('B \n', B)
+    # print('B的维度:', B.shape)
 
-# start_time = datetime.datetime.now()
-start_time = time.time()
-combined_inv = ca.pinv(B)
-print("combined inv:", combined_inv)
-print("combined inv shape:", combined_inv.shape)
-# end_time = datetime.datetime.now()
-t_ = time.time()
-print('time for inverse \n', t_-start_time)
+    # C = np.reshape(B,(5,12))
+    # print('C \n', C)
+    # print('C的维度:', C.shape)
 
-# G_ref = ca.mtimes(combined_inv, stacked)
-# print("G_ref shape:", G_ref.shape)
-# r_g = lambda_g*ca.norm_2(G-G_ref)
-# print("r(g) shape:", r_g.shape)
+    # start_time = datetime.datetime.now()
+    # start_time = time.time()
+    combined_inv = ca.pinv(B)
+    print("combined inv:", combined_inv)
+    print("combined inv shape:", combined_inv.shape)
+    # # end_time = datetime.datetime.now()
+    # t_ = time.time()
+    # print('time for inverse \n', t_-start_time)
+
+    # G_ref = ca.mtimes(combined_inv, stacked)
+    # print("G_ref shape:", G_ref.shape)
+    # r_g = lambda_g*ca.norm_2(G-G_ref)
+    # print("r(g) shape:", r_g.shape)
